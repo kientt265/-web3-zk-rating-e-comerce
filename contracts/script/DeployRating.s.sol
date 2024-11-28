6cfcbabf83ab063ff5016fb7a6b66f72baaa8483
kientt265 <kientran26503@gmail.com>
2024-11-28 15:35:05 +0700

    add-Rating-contract

---
+++ contracts/script/DeployRating.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+import {Rating} from "../src/Rating.sol";
+
+contract DeployRating is Script {
+    Rating public rating;
+
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+
+        rating = new Rating();
+
+        vm.stopBroadcast();
+    }
+}
