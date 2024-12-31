2ac531a7936cc8516ad6f0df408f125a95da37ce
hieundt <nduongtrunghieu@gmail.com>
2024-12-31 12:59:27 +0700

    Request

--- back/src/service/request.service.js
+++ back/src/service/request.service.js
@@ -8,4 +8,4 @@ export const getRequestService = async (dealId) => {
   const { blockNumber, proofMerkle, ...rest } = data; // Cu phap destruct cua JS, chia ra 2 phan blockNumber va proofMerkle, con lai la rest
 
   return { blockNumber, proofMerkle }; // Tra ve blockNumber va proofMerkle duoi dang Oject
-}
+};
